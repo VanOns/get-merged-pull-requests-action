7f8befcad19edf19afd474a4a344a5453064d2ff
dnwjn <hello@dnwjn.dev>
2023-08-15 20:16:44 +0200

    [Feat] Check correct variable based on env

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,12 +3,18 @@ import * as cp from 'child_process'
 import * as path from 'path'
 import {test, expect} from '@jest/globals'
 import * as dotenv from 'dotenv'
+import * as github from '@actions/github'
 
 dotenv.config({path: '.env.test'})
 
 test('test runs', () => {
   expect(process.env['INPUT_GITHUB_TOKEN']).not.toBeUndefined()
-  expect(process.env['INPUT_REPO']).not.toBeUndefined()
+
+  if (process.env['GITHUB_ACTIONS']) {
+    expect(github.context.repo).not.toBeUndefined()
+  } else {
+    expect(process.env['INPUT_REPO']).not.toBeUndefined()
+  }
 
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
