83e8e7048ee34a59739c7eab1cd220653c302589
dnwjn <hello@dnwjn.dev>
2023-08-15 20:03:00 +0200

    [Feat] Add env to testing

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,14 +1,14 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {test} from '@jest/globals'
+import {test, expect} from '@jest/globals'
+import * as dotenv from 'dotenv'
+
+dotenv.config({path: '.env.test'})
 
 test('test runs', () => {
-  // process.env['INPUT_GITHUB_TOKEN'] = ''
-  // process.env['INPUT_REPO'] = ''
-  // process.env['INPUT_CURRENT_TAG'] = ''
-  // process.env['INPUT_PREVIOUS_TAG'] = ''
-  // process.env['INPUT_RETURN_TYPE'] = ''
+  expect(process.env['INPUT_GITHUB_TOKEN']).not.toBeUndefined()
+  expect(process.env['INPUT_REPO']).not.toBeUndefined()
 
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
